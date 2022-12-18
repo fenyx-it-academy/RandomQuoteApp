0d9fbc35f5322d06136dcb85d4abd5703225a2ad
Rama Nahawandi <rama_nahawandi@outlook.com>
2022-12-18 14:11:11 +0100

    "finalizing the project"

--- main.py
+++ main.py
@@ -2,37 +2,17 @@ from tkinter import *
 import requests
 
 def get_quote():
-    
-
     try:
-        r = requests.get("https://api.quotable.io/random?tags=technology,famous-quotes")
+        r = requests.get("https://api.quotable.io/random?maxLength=50")
         responsejson=r.json()
         quote=responsejson["content"]
         author=responsejson["author"]     
     except requests.ConnectionError as e:
         print(e)
-    
 
 
-
-
-    # TODO!
-    #Write your code here.
-
-    #Make an API call to get a random quote (including exception handling)
-    # Requirements of the API call:
-        # Make the call to 'http://api.quotable.io'
-        # Get 1 random quote at a time
-        # Specify a category (check out the documentation to learn more about categories and how to use them)
-        # Customize your endpoint and parameters according to the requirements above.  
-    #Parse the response object and store the quote and the author in variables called 'quote' and 'author'
-
-    #Pass the quote variable into the canvas via the 'quote' variable.
     canvas.itemconfig(quote_text, text=quote)
-    #Pass the author variable into the canvas via the 'author' variable.
     canvas.itemconfig(author_text, text=author)
-
-
 
 window = Tk()
 window.title("Random Quote App")
@@ -46,18 +26,16 @@ quote_text = canvas.create_text(
     150,
     180,
     text="Click on the button below to load a random quote.",
-    width=250,
+    width=200,
     font=("Arial", 30, "bold"),
     fill="white",
 )
 canvas.grid(row=0, column=0)
 
 author_text = canvas.create_text(
-    150, 320, text="", width=250, font=("Arial", 20, "italic"), fill="white"
+    150, 350, text="", width=250, font=("Arial", 20, "italic"), fill="white"
 )
 canvas.grid(row=0, column=0)
-
 button = Button(highlightthickness=0, command=get_quote, text="Get a new quote")
 button.grid(row=1, column=0)
-
 window.mainloop()
