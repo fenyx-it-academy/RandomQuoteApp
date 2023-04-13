f85a08e915af0c08b68acf3de5575f362b047b94
elen-ladder <elen.ladder@gmail.com>
2023-04-13 12:48:59 +0200

    HW

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
 from tkinter import *
-# TODO! 
-# make necessary imports for making API calls
+import requests
 
 def get_quote():
-    pass
-    # TODO!
-    #Make an API call to get a random quote (including exception handling)
-    # Requirements of the API call:
-        # Make the call to 'http://api.quotable.io' (API documentation => https://github.com/lukePeavey/quotable#readme)
-        # Get 1 random quote at a time
-        # Specify a category (check out the documentation to learn more about categories and how to use them)
-        # Customize your endpoint and parameters according to the requirements above.  
- 
-    # <YOUR CODE HERE>    
+   
+    response = requests.get("http://api.quotable.io/random?maxLength=150&tags=stupidity|wisdom")
+    quote = response.json()["content"]
+    author = response.json()["author"]
         
-    #Parse the response object and store the quote and the author in variables called 'quote' and 'author'
-
-    # <YOUR CODE HERE>    
-    
-    #Pass the quote variable into the canvas via the 'quote' variable.
     canvas.itemconfig(quote_text, text=quote)
-    #Pass the author variable into the canvas via the 'author' variable.
     canvas.itemconfig(author_text, text=author)
-
+    
 
 
 window = Tk()
@@ -38,13 +25,13 @@ quote_text = canvas.create_text(
     180,
     text="Click on the button below to load a random quote.",
     width=250,
-    font=("Arial", 30, "bold"),
+    font=("Arial", 18, "bold"),
     fill="white",
 )
 canvas.grid(row=0, column=0)
 
 author_text = canvas.create_text(
-    150, 320, text="", width=250, font=("Arial", 20, "italic"), fill="white"
+    150, 320, text="", width=250, font=("Arial", 15, "italic"), fill="white"
 )
 canvas.grid(row=0, column=0)
 
